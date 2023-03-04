483ebb321585483c5de3a093f83eeb35e9383d92
Aleksandra0602 <aleksandraniewiadoma0602@gmail.com>
2023-03-04 15:24:49 +0100

    poprawa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zadanie_rek/UI/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'UI/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: HomePage(),
     );
   }
